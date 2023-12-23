8c38fbafc4a07645834dec6a4d5ac15c8f0873e0
Barry <ezeanibarry1@gmail.com>
2023-12-23 07:01:26 +0100

    Using all the techniques to avoid using global variables on our code and placing it in an external file.

---
+++ Simple JS Slideshow Activity/script.js
@@ -0,0 +1,34 @@
+(function () {
+  const myimages = [
+    "image1.jpg",
+    "image2.jpg",
+    "image3.jpg",
+    "image4.jpg",
+    "image5.jpg",
+  ];
+  
+  let currentImage = 0;
+  
+  function nextPhoto() {
+    currentImage++;
+    if (currentImage > myimages.length - 1) {
+      currentImage = 0;
+    }
+    document.querySelector("#myimage").src = myimages[currentImage];
+  }
+  
+  function previousPhoto() {
+    if (currentImage === 0) {
+      currentImage = myimages.length - 1;
+    } else {
+      currentImage--;
+    }
+  
+    document.querySelector("#myimage").src = myimages[currentImage];
+  }
+  
+  document.querySelector("#next").addEventListener("click", nextPhoto);
+  document
+    .querySelector("#previous")
+    .addEventListener("click", previousPhoto);
+})(); 
