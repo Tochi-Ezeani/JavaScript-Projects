8689b1aad143de0104720f50dead6743ead8a9a9
Barry <ezeanibarry1@gmail.com>
2023-12-31 08:58:35 +0100

    Using functions to avoid redundant code

--- SLIDESHOW WITH CROSS FADE EFFECT ACTIVITY/script.js
+++ SLIDESHOW WITH CROSS FADE EFFECT ACTIVITY/script.js
@@ -12,14 +12,7 @@
   const nextBtn = document.querySelector("#next");
   const prevBtn = document.querySelector("#previous");
 
-  nextBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    currentImage++;
-    if (currentImage > myphotos.length - 1) {
-      currentImage = 0;
-    }
-
+  function imgCreatorAndRegulator() {
     let newSlide = document.createElement("img");
     newSlide.className = "fadeinimg";
     newSlide.src = `slides/${myphotos[currentImage]}`;
@@ -28,6 +21,16 @@
     if (container.children.length > 2) {
       container.removeChild(container.children[0]);
     }
+  }
+
+  nextBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    currentImage++;
+    if (currentImage > myphotos.length - 1) {
+      currentImage = 0;
+    }
+    imgCreatorAndRegulator();
   });
 
   prevBtn.addEventListener("click", function (event) {
@@ -37,14 +40,6 @@
     if (currentImage < 0) {
       currentImage = myphotos.length - 1;
     }
-
-    let newSlide = document.createElement("img");
-    newSlide.className = "fadeinimg";
-    newSlide.src = `slides/${myphotos[currentImage]}`;
-    container.appendChild(newSlide);
-
-    if (container.children.length > 2) {
-      container.removeChild(container.children[0]);
-    }
+    imgCreatorAndRegulator();
   });
 })();
